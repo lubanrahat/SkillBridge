42870f74b24034c7c9932e1d79280f3bd216c433
lubanrahat <lubanrahat.dev@gmail.com>
2026-01-30 22:43:20 +0600

    added seed admin

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,32 @@
+import { PrismaClient, Role } from "../src/generated/prisma/client";
+import bcrypt from "bcryptjs";
+import { prisma } from "../src/lib/prisma";
+
+
+async function main() {
+  console.log("🌱 Seeding admin user...");
+
+  const password = await bcrypt.hash("Admin@123", 10);
+
+  const admin = await prisma.user.upsert({
+    where: { email: "admin@skillbridge.com" },
+    update: {},
+    create: {
+      email: "admin@skillbridge.com",
+      password,
+      name: "Admin User",
+      role: Role.ADMIN,
+    },
+  });
+
+  console.log("✅ Admin created:", admin.email);
+}
+
+main()
+  .catch((e) => {
+    console.error("❌ Seed failed:", e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
